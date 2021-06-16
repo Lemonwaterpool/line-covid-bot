1052d4394c9ad7d02ba3dfd9cc5687d0c2d1338f
Lemonwaterpool <frqk3624@gmail.com>
2021-06-17 01:23:42 +0900

    fix

--- src/reply/ButtonOrErrorMessage.js
+++ src/reply/ButtonOrErrorMessage.js
@@ -7,7 +7,7 @@ exports.SendMessage = (client, event) => {
 
     if (text === '今日の感染者数') {
         client.replyMessage(replyToken, TextMessageTemplate.Template('照会したい都道府県を送信してください。'));
-    } else if (AreaList.includes(text)) {
+    } else if (AreaList.indexOf(text) >= 0) {
         client.replyMessage(replyToken, TextMessageTemplate.Template(`${text}のデータを照会中です。。。`));
     } else {
         client.replyMessage(replyToken, TextMessageTemplate.Template('エラーです。'));
